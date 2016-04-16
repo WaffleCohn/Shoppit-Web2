87d518204151d3cac17fc6915ceec0f8a18df36a
Aditya Vaidya <kroq.gar78@gmail.com>
2016-04-16 16:06:42 -0500

    Working Profile model

--- thelist/models.py
+++ thelist/models.py
@@ -1,10 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 import uuid
 
-# class User(models.Model):
-    # name = models.CharField(max_length=30)
-    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    # credit_card = models.CharField(max_length=20)
-    # email = models.EmailField()
-    # home_loc = models.CharField(max_length=100)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    credit_card = models.CharField(max_length=20)
+    home_loc = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.username
+
+# @static_method
+def create_profile(username, email, password=None):
+    user = User.objects.create_user(username, email, password)
+    user.save()
+
+    profile = Profile(user=user)
+    profile.save()
+    user.save()
+    return profile
--- thelist/views.py
+++ thelist/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
+from django.conf import settings
+from django.conf.urls.static import static
+from django.shortcuts import render, render_to_response
+from django.template.context_processors import csrf
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.core import serializers
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models import Max, Avg, Min
 
-# Create your views here.
+def view_user(request):
+    return HttpResponse("asdf")
