8aaf3f8353741f87938da4e8efa6d3882c68af66
Aditya Vaidya <kroq.gar78@gmail.com>
2016-04-16 17:44:42 -0500

    Add untested Item and Transaction models

--- thelist/models.py
+++ thelist/models.py
@@ -7,9 +7,20 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     credit_card = models.CharField(max_length=20)
     home_loc = models.CharField(max_length=100)
+    rating = models.FloatField(default=2.5)
 
     def __str__(self):
         return self.user.username
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+
+class Transaction(models.Model):
+    value = models.DecimalField(default=1,max_digits=100,decimal_places=10)
+    items = models.ManyToManyField(Item)
+    seller = models.ForeignKey(Profile, related_name='+', on_delete=models.CASCADE)
+    buyer = models.ForeignKey(Profile, related_name='+', on_delete=models.CASCADE)
+    datetime = models.DateTimeField(auto_now_add=True)
 
 # @static_method
 def create_profile(username, email, password=None):
