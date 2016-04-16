3a52d0d4d558f75b1c89fc09a1fcf7c48ddc0525
Aditya Vaidya <kroq.gar78@gmail.com>
2016-04-16 17:07:52 -0500

    Basic view user view

--- thelist/views.py
+++ thelist/views.py
@@ -4,10 +4,15 @@ from django.conf.urls.static import static
 from django.shortcuts import render, render_to_response
 from django.template.context_processors import csrf
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_GET, require_POST
 from django.core import serializers
 from django.core.serializers.json import DjangoJSONEncoder
 from django.db.models import Max, Avg, Min
+from django.contrib.auth.models import User
+
+from .models import *
 
 def view_user(request):
-    return HttpResponse("asdf")
+    username = request.GET.get("username")
+    profile = User.objects.get(username=username).profile
+    return HttpResponse(profile.user.username)
