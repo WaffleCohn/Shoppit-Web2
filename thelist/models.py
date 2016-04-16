8e434d07bffb204fafc994fd527afe2668d5f689
Aditya Vaidya <kroq.gar78@gmail.com>
2016-04-16 16:05:54 -0500

    Initial commit with some stuff

---
+++ thelist/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+import uuid
+
+# class User(models.Model):
+    # name = models.CharField(max_length=30)
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # credit_card = models.CharField(max_length=20)
+    # email = models.EmailField()
+    # home_loc = models.CharField(max_length=100)
